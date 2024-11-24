83eb1563b0b3be3dda550fb176ca8a2cc403d4a8
pedropinrodrigues <pedro.rodrigues@sou.inteli.edu.br>
2024-11-24 07:46:51 -0300

    commit

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from generate_introductions import app as introductions_app
 from generate_roadmap import app as roadmap_app
 from generate_content import app as content_app  # Importa o app de generate_content
+from generate_chatbot import app as chatbot_app  # Importa o app de generate_chatbot
 from fastapi.middleware.cors import CORSMiddleware
 
 # Cria o app principal
@@ -10,6 +11,7 @@ app = FastAPI()
 
 # Configure CORS
 # Inclui os roteadores de todos os módulos
+app.include_router(chatbot_app.router, prefix="/chatbot")
 app.include_router(introductions_app.router, prefix="/introductions")
 app.include_router(roadmap_app.router, prefix="/roadmaps")
 app.include_router(content_app.router, prefix="/content")  # Adiciona o roteador de generate_content
